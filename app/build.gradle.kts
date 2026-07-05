87231846ef76477f493a21cb92078dddf2bcf95a
Leonov792 <Leonov792@users.noreply.github.com>
2026-07-05 20:16:58 +0300

    Simplify: remove GMS/MPAndroid dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,5 +12,4 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0"); implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.room:room-runtime:2.6.1"); implementation("androidx.room:room-ktx:2.6.1"); ksp("androidx.room:room-compiler:2.6.1")
-    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
